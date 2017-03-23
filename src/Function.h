9d334e1daf5f0937624d501a850f7519dd45859c
ishmum <ishmum.jawad@aplombtechbd.com>
2017-03-23 12:33:13 +0600

    FunctionFactory Changed and MoveListGenerator Created

--- src/Function.h
+++ src/Function.h
@@ -6,91 +6,72 @@
 class Function { public: virtual double run() = 0; };
 
 class OneArgFunction : public Function {
-public: OneArgFunction(double d1) : d1 (d1) {}
 protected: double d1;
+public: void initiate(double d1) { this->d1 = d1; }
 };
 
 class DoNothingFunction : public OneArgFunction {
-public: 
-	DoNothingFunction(double d1) : OneArgFunction(d1) {}
-	double run() { return d1; }
+public: double run() { return d1; }
 };
 
 class InvertValueFunction : public OneArgFunction {
-public:
-	InvertValueFunction(double d1) : OneArgFunction(d1) {}
-	double run() { return -d1; }
+public: double run() { return -d1; }
 };
 
 class LogFunction : public OneArgFunction {
-public: 
-	LogFunction(double d1) : OneArgFunction(d1) {}
-	double run() { return log(d1); }
+public: double run() { return log(d1); }
 };
 
 class TwoArgFunction : public Function {
-public: TwoArgFunction(double d1, double d2) : d1 (d1), d2 (d2) {}
 protected: double d1, d2;
+public: 
+	void initiate(double d1, double d2) {
+		this->d1 = d1;
+		this->d2 = d2;
+	}
 };
 
 class AddFunction : public TwoArgFunction { 
-public: 
-	AddFunction(double d1, double d2) : TwoArgFunction(d1, d2) {}
-	double run() { return (d1 + d2); } 
+public: double run() { return (d1 + d2); } 
 };
 
 class SubtractFunction : public TwoArgFunction { 
-public: 
-	SubtractFunction(double d1, double d2) : TwoArgFunction(d1, d2) {}
-	double run() { return (d1 - d2); } 
+public: double run() { return (d1 - d2); } 
 };
 
 class MultiplyFunction : public TwoArgFunction { 
-public: 
-	MultiplyFunction(double d1, double d2) : TwoArgFunction(d1, d2) {}
-	double run() { return (d1 * d2); } 
+public: double run() { return (d1 * d2); } 
 };
 
 class DivideFunction : public TwoArgFunction { 
-public: 
-	DivideFunction(double d1, double d2) : TwoArgFunction(d1, d2) {}
-	double run() { return (d1 / d2); } 
+public: double run() { return (d1 / d2); } 
 };
 
-//TODO: A Factory Pattern Might Be Redundant Here If No Extension Is Required
 class FunctionFactory {
-	double result;
-	double calculateResultAndRemovePointer(Function *f);
 public:
-	double getResult(int i, double d);
-	double getResult(int i, double d1, double d2);
+	OneArgFunction * getOneArgFunction(int i);
+	TwoArgFunction * getTwoArgFunction(int i);
+	int getNumOfOneArgFunction() { return 3; }
+	int getNumOfTwoArgFunction() { return 4; }
 };
 
-double calculateResultAndRemovePointer(Function *f) {
-	double result = f->run();
-	delete f;
-	return result; 
+OneArgFunction * FunctionFactory::getOneArgFunction(int i) {
+	switch (i) {
+		case 0: return new DoNothingFunction();
+		case 1: return new InvertValueFunction();
+		case 2: return new LogFunction();
+		default: throw -1;
+	}
 }
 
-double FunctionFactory::getResult(int i, double d) {
-	Function *f;
-	switch i {
-		case 1: f = new DoNothingFunction(d);
-		case 2: f = new InvertValueFunction(d);
-		case 3: f = new LogFunction(d);
+TwoArgFunction * FunctionFactory::getTwoArgFunction(int i) {
+	switch (i) {
+		case 0: return new AddFunction();
+		case 1: return new SubtractFunction();
+		case 2: return new MultiplyFunction();
+		case 3: return new DivideFunction();
+		default: throw -1;
 	}
-	return calculateResultAndRemovePointer(f);
-}
-
-double FunctionFactory::getResult(int i, double d1, double d2) {
-	Function *f;
-	switch i {
-		case 1: f = new AddFunction(d1, d2);
-		case 2: f = new SubtractFunction(d1, d2);
-		case 3: f = new MultiplyFunction(d1, d2);
-		case 4: f = new DivideFunction(d1, d2);
-	}
-	return calculateResultAndRemovePointer(f);
 }
 
 #endif
--- src/MoveListGenerator.h
+++ src/MoveListGenerator.h
@@ -1,72 +1,90 @@
 #ifndef __MOVE_GENERATOR_H__
 #define __MOVE_GENERATOR_H__
 
+#include <iostream>
 #include <vector>
+#include <stdlib.h>
 #include "Function.h"
+#include <time.h>
 
 using namespace std;
 
 struct Move {
-	unsigned short funcNum;
+	Move(Function *f) : f (f) {}
+	Function *f;
 };
 
 struct TwoArgMove : public Move {
+	TwoArgMove(Function *f, int i, int j) : Move(f), paramOne (i), paramTwo (j) {}
 	int paramOne;
 	int paramTwo;
 };
 
 struct OneArgMove : public Move {
+	OneArgMove(Function *f, int i) : Move(f), paramOne (i) {}
 	int paramOne;
 };
 
 class MoveListGenerator {
 	vector<double> vd;
 	vector<Move> vm;
+	FunctionFactory ff;
 	int numOneArgFunc, numTwoArgFunc, x, y;
-	void swapVal(int *x, int *y);
-	void applyMovesToVector();
+	void generateOneArgFunction(int i);
+	void swapVal();
+	void generateTwoRandomInts();
+	void generateTwoArgFunction();
+	void processTwoArgFunction(TwoArgFunction *f);
+	void generateMoveList();
 public:
-	MoveListGenerator(int i);
+	MoveListGenerator(int numVal, int j, int k);
+	vector<double> getDoubleVector() { return vd; }
 };
 
-void MoveListGenerator::applyTwoArgMove() {
-	TwoArgMove m = {(rand() % k), vd.at(x), vd.at(y)};
-	vd.push_back(ff.getResult(m.funcNum, vd.at(m.paramOne), vd.at(m.paramTwo)));
-	vd.erase(vd.begin() + y);
-	vd.erase(vd.begin() + x);	
-        vm.push_back(m);
-}
-
-void MoveListGenerator::getTwoRandomNumbers() {
-	x = (rand() % (vd.size() - 1));
-	y = (rand() % (vd.size() - 1));
-	while (x == y) y = (rand() % (vd.size() - 1));
-	if (y < x) swapVal(&x, &y);
-}
-
-void MoveListGenerator::applyOneArgMove(int i, int j) {
-	OneArgMove m {(rand() % j), i};
-	vd.at(i) = ff.getResult(m.funcNum, vd.at(i));
+void MoveListGenerator::generateOneArgFunction(int i) {
+	OneArgFunction *f = ff.getOneArgFunction(rand() % numOneArgFunc);
+	OneArgMove m {f, i};
 	vm.push_back(m);
+	f->initiate(vd.at(i));
+	vd.at(i) = f->run();
 }
 
-void MoveListGenerator::applyMovesToVector() {
-	for (int i = 0; i < vd.size(); i++) applyOneArgMove(i, j);
-	getTwoRandomNumbers();
-	applyTwoArgMove();
-}
-
-void MoveListGenerator::swapVal(int *x, int *y) {
+void MoveListGenerator::swapVal() {
 	int tmp = x;
 	x = y;
 	y = tmp;
 }
 
-MoveListGenerator(int numVal, int j, int k) : numOneArgFunc (j), numTwoArgFunc (k) { 
+void MoveListGenerator::generateTwoRandomInts() {
+	x = (rand() % vd.size());
+	y = (rand() % vd.size());
+	while (x == y) y = (rand() % vd.size());
+	if (y < x) swapVal();
+}
+
+void MoveListGenerator::processTwoArgFunction(TwoArgFunction *f) {
+	f->initiate(vd.at(x), vd.at(y));
+	vd.push_back(f->run());
+	vd.erase(vd.begin() + y);
+	vd.erase(vd.begin() + x);
+}
+
+void MoveListGenerator::generateTwoArgFunction() {
+	TwoArgFunction *f = ff.getTwoArgFunction(rand() % numTwoArgFunc);
+	TwoArgMove m {f, x, y};
+	vm.push_back(m);
+	processTwoArgFunction(f);
+}
+
+void MoveListGenerator::generateMoveList() {
+	for (int i = 0; i < (vd.size() - 1); i++) generateOneArgFunction(i);
+	if (vd.size() > 1) generateTwoArgFunction();
+}
+
+MoveListGenerator::MoveListGenerator(int numVal, int j, int k) : numOneArgFunc (j), numTwoArgFunc (k) { 
 	srand(time(0));
 	for (int i = 0; i < numVal; i++) vd.push_back(1); 
-	FunctionFactory ff;
-	while (vd.size() > 1) applyMovesToVector();
+	while (vd.size() > 1) generateMoveList();
 }
 
 #endif
